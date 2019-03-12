8a8c3fc370d2f37e4b97d4ab171c0a47e12cca45
phanna <studentphanna@gmail.com>
2019-03-12 09:05:37 +0700

    Added custom field in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
    protected
    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:shop_name])
    end
 end
