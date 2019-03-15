1d5c83fd065b37d491b285b54b29863998da460a
Khom Sovon <sovon.khom68@gmail.com>
2019-03-15 17:20:33 +0700

    Add active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
    before_action  :configure_permitted_parameters, if: :devise_controller?
 
    protected
+
    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:shop_name])
