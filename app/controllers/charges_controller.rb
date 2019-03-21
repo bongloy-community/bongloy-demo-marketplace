0d6cc354fdae3fad623c7d6f968de8aace96cb6b
Khom Sovon <sovon.khom68@gmail.com>
2019-03-21 23:33:34 +0700

    charge [wip]

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,25 @@
 class ChargesController < ApplicationController
-  def new
-    @product = Product.find(params[:product_id])
+  before_action :set_product, only: %i[new create]
+
+  def new; end
+
+  def create
+
+    charge = Stripe::Charge.create(
+      :source => params[:token],
+      :amount      => @product.price,
+      :description => "#{@product.description}",
+      :currency    => 'USD',
+      :destination => {
+        :amount => @product.price,
+        :account => @product.user.stripe_user_id,
+      }
+    )
   end
 
-  def create; end
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
 end
