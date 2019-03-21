a4b5d8e64a0508919f7b59b6a4c7ceebe9642ef9
phanna <studentphanna@gmail.com>
2019-03-21 18:21:48 +0700

    add features spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,18 @@
 FactoryBot.define do
-
   factory :user do
-    email { "Plus" }
+    email { "phanna@bongloy.com" }
     shop_name { "Android" }
-    encrypted_password { "12345678" }
+    password { "12345678" }
   end
 
   factory :product do
-    name { "OnePlust" }
-    description { "new smart phone technology" }
+    name { "OnePlus" }
+    description { "smart phone" }
     price { 200 }
+    trait :with_cover_product do
+      after(:build) do |product|
+        product.cover_product.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'dummy_attachment.jpg')), filename: 'dummy_attachment.jpg', content_type: 'image/jpg')
+      end
+    end
   end
 end
