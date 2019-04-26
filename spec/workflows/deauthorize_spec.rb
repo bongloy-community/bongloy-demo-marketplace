c5dc7f746ca7c7bfcb73dfe38f88ccde4cf96829
phanna <studentphanna@gmail.com>
2019-04-26 15:41:55 +0700

    test deauthorized workflows

--- spec/workflows/deauthorize_spec.rb
+++ spec/workflows/deauthorize_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Deauthorize do
   describe "#run" do
+    let(:deauthorized) { instance_double(deauthorize_access) }
     it "sets Stripe Account to nil for current user" do
-      current_user = create(:user, stripe_account_id: "valid")
+      current_user = create(:user)
 
-      workflow = described_class.new(user: current_user)
+      workflow = described_class.new(current_user)
 
-      allow(Deauthorize).to receive(:run).and_return(current_user)
+      allow(workflow).to receive(:deauthorize_access).and_return({ stripe_user_id: "acct_12345678" })
+
+      workflow.run
 
       expect(current_user.stripe_account_id).to eq nil
     end
