1c13abe59c2c06c6528457caaec1e1f69a214369
Khom Sovon <sovon.khom68@gmail.com>
2019-05-16 10:55:49 +0700

    add compile true

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 #
 Rails.application.config.assets.precompile += %w[payment_form payment_form.css]
+Rails.application.config.assets.compile = true
